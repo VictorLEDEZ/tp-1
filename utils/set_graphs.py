299ae115a23c836917a30b61d29dbf1d406a4c7f
Victor LEDEZ <ledezvictor@gmail.com>
2021-10-01 20:39:03 +0200

    changing names

--- utils/set_graphs.py
+++ utils/set_graphs.py
@@ -14,7 +14,7 @@ def title(mu1, sigma1, mu2, sigma2):
     """
     return 'mu1 = ' + str(mu1) + ', sigma1 = ' + str(sigma1) + '  |  ' + 'mu2 = ' + str(mu2) + ', sigma2 = ' + str(sigma2)
 
-def plot_results(signal_number, errors_1, errors_2, all_mu_1, all_sigma_1, all_mu_2, all_sigma_2):
+def plot_results(signal_number, all_gaussian_errors, all_mpm_errors, all_mu_1, all_sigma_1, all_mu_2, all_sigma_2):
     """plots the results thanks to pyplot
 
     Args:
@@ -28,29 +28,29 @@ def plot_results(signal_number, errors_1, errors_2, all_mu_1, all_sigma_1, all_m
     fig, axs = plt.subplots(3, 2)
     fig.suptitle('signal = ' + str(signal_number))
 
-    axs[0, 0].plot(errors_1[0])
-    axs[0, 0].plot(errors_2[0], 'r')
+    axs[0, 0].plot(all_gaussian_errors[0])
+    axs[0, 0].plot(all_mpm_errors[0], 'r')
     axs[0, 0].set_title(title(all_mu_1[0], all_sigma_1[0], all_mu_2[0], all_sigma_2[0]))
-    axs[0, 0].set(ylabel = str(round(errors_1[0][-1], 3)))
+    axs[0, 0].set(ylabel = str(round(all_gaussian_errors[0][-1], 3)))
 
-    axs[0, 1].plot(errors_1[1])
-    axs[0, 1].plot(errors_2[1], 'r')
+    axs[0, 1].plot(all_gaussian_errors[1])
+    axs[0, 1].plot(all_mpm_errors[1], 'r')
     axs[0, 1].set_title(title(all_mu_1[1], all_sigma_1[1], all_mu_2[1], all_sigma_2[1]))
-    axs[0, 1].set(ylabel = str(round(errors_1[1][-1], 3)))
+    axs[0, 1].set(ylabel = str(round(all_gaussian_errors[1][-1], 3)))
 
-    axs[1, 0].plot(errors_1[2])
-    axs[1, 0].plot(errors_2[2], 'r')
+    axs[1, 0].plot(all_gaussian_errors[2])
+    axs[1, 0].plot(all_mpm_errors[2], 'r')
     axs[1, 0].set_title(title(all_mu_1[2], all_sigma_1[2], all_mu_2[2], all_sigma_2[2]))
-    axs[1, 0].set(ylabel = str(round(errors_1[2][-1], 3)))
+    axs[1, 0].set(ylabel = str(round(all_gaussian_errors[2][-1], 3)))
 
-    axs[1, 1].plot(errors_1[3])
-    axs[1, 1].plot(errors_2[3], 'r')
+    axs[1, 1].plot(all_gaussian_errors[3])
+    axs[1, 1].plot(all_mpm_errors[3], 'r')
     axs[1, 1].set_title(title(all_mu_1[3], all_sigma_1[3], all_mu_2[3], all_sigma_2[3]))
-    axs[1, 1].set(ylabel = str(round(errors_1[3][-1], 3)))
+    axs[1, 1].set(ylabel = str(round(all_gaussian_errors[3][-1], 3)))
 
-    axs[2, 0].plot(errors_1[4])
-    axs[2, 0].plot(errors_2[4], 'r')
+    axs[2, 0].plot(all_gaussian_errors[4])
+    axs[2, 0].plot(all_mpm_errors[4], 'r')
     axs[2, 0].set_title(title(all_mu_1[4], all_sigma_1[4], all_mu_2[4], all_sigma_2[4]))
-    axs[2, 0].set(ylabel = str(round(errors_1[4][-1], 3)))
+    axs[2, 0].set(ylabel = str(round(all_gaussian_errors[4][-1], 3)))
 
     plt.show()
