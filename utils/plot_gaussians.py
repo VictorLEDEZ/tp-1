11c86eb5d97d96e5296a0bd642ae6cace616645f
Victor LEDEZ <ledezvictor@gmail.com>
2021-10-04 21:42:43 +0200

    added title.py

--- utils/plot_gaussians.py
+++ utils/plot_gaussians.py
@@ -1,24 +1,11 @@
 from matplotlib import pyplot as plt
 import scipy.stats as stats
 import numpy as np
-import math
+
+from .title import title
 
 def calculate_x(mu, sigma):
     return np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
-
-def title(mu1, sigma1, mu2, sigma2):
-    """generates the title string
-
-    Args:
-        mu1 (Number): the first mean
-        sigma1 (Number): the first standard deviation
-        mu2 (Number): the second mean
-        sigma2 (Number): the second standard deviation
-
-    Returns:
-        String: the generated string
-    """
-    return 'mu1 = ' + str(mu1) + ', sigma1 = ' + str(sigma1) + '  |  ' + 'mu2 = ' + str(mu2) + ', sigma2 = ' + str(sigma2)
 
 def plot_gaussians(all_mu_1, all_sigma_1, all_mu_2, all_sigma_2):
     """plots the results thanks to pyplot
--- utils/set_graphs.py
+++ utils/set_graphs.py
@@ -1,18 +1,6 @@
 from matplotlib import pyplot as plt
 
-def title(mu1, sigma1, mu2, sigma2):
-    """generates the title string
-
-    Args:
-        mu1 (Number): the first mean
-        sigma1 (Number): the first standard deviation
-        mu2 (Number): the second mean
-        sigma2 (Number): the second standard deviation
-
-    Returns:
-        String: the generated string
-    """
-    return 'mu1 = ' + str(mu1) + ', sigma1 = ' + str(sigma1) + '  |  ' + 'mu2 = ' + str(mu2) + ', sigma2 = ' + str(sigma2)
+from .title import title
 
 def plot_results(signal_number, all_gaussian_errors, all_mpm_errors, all_mu_1, all_sigma_1, all_mu_2, all_sigma_2):
     """plots the results thanks to pyplot
