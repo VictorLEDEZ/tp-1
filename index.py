8acc288f8a2f473a0afc05be26eabf5142301ccc
Victor LEDEZ <ledezvictor@gmail.com>
2021-10-05 20:48:03 +0200

    added start calculations

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 # Importing our modules
-from utils.classes_calculations import classes_calculations
+from utils.start_calculations import start_calculations
 from utils.simulate_signal import simulate_signals
 from utils.plot_gaussians import plot_gaussians
-from utils.set_graphs import plot_results
-from utils.mean_error import mean_error
 from utils.signals import load_signals
 
 # Importing all the variables
@@ -18,40 +16,10 @@ from variables import MU1
 from variables import MU2
 
 # Import all the signals and storing them in an array
-signals = load_signals()
 simulated_signals = simulate_signals(SIMULATION_SIZE, CLASS1, CLASS2, P1_P2)
+signals = load_signals()
 
 plot_gaussians(MU1, SIGMA1, MU2, SIGMA2)
 
-signal_number = 0
-simulated_signal_number = 0
-
-# for signal in signals:
-
-#     signal_number += 1 
-
-#     c1, c2 = classes_calculations(signal)
-#     all_guassian_errors = []
-#     all_mpm_errors = []
-
-#     for i in range(0, len(MU1)):
-#         E_gaussian, E_mpm = mean_error(ERROR_ITERATIONS, signal, c1, c2, MU1[i], SIGMA1[i], MU2[i], SIGMA2[i])
-#         all_guassian_errors.append(E_gaussian)
-#         all_mpm_errors.append(E_mpm)
-
-#     plot_results(signal_number, all_guassian_errors, all_mpm_errors, MU1, SIGMA1, MU2, SIGMA2)
-
-for simulated_signal in simulated_signals:
-
-    simulated_signal_number += 1 
-
-    c1, c2 = classes_calculations(simulated_signal)
-    all_guassian_errors = []
-    all_mpm_errors = []
-
-    for i in range(0, len(MU1)):
-        E_gaussian, E_mpm = mean_error(ERROR_ITERATIONS, simulated_signal, c1, c2, MU1[i], SIGMA1[i], MU2[i], SIGMA2[i])
-        all_guassian_errors.append(E_gaussian)
-        all_mpm_errors.append(E_mpm)
-
-    plot_results(simulated_signal_number, all_guassian_errors, all_mpm_errors, MU1, SIGMA1, MU2, SIGMA2)
+start_calculations(signals, ERROR_ITERATIONS, MU1, SIGMA1, MU2, SIGMA2)
+start_calculations(simulated_signals, ERROR_ITERATIONS, MU1, SIGMA1, MU2, SIGMA2)
---
+++ utils/start_calculations.py
@@ -0,0 +1,23 @@
+from utils.classes_calculations import classes_calculations
+from utils.set_graphs import plot_results
+from utils.mean_error import mean_error
+
+
+def start_calculations(signals, error_iterations, mus1, sigmas1, mus2, sigmas2):
+
+    signal_number = 0
+
+    for signal in signals:
+
+        signal_number += 1 
+
+        c1, c2 = classes_calculations(signal)
+        all_guassian_errors = []
+        all_mpm_errors = []
+
+        for i in range(0, len(mus1)):
+            E_gaussian, E_mpm = mean_error(error_iterations, signal, c1, c2, mus1[i], sigmas1[i], mus2[i], sigmas2[i])
+            all_guassian_errors.append(E_gaussian)
+            all_mpm_errors.append(E_mpm)
+
+        plot_results(signal_number, all_guassian_errors, all_mpm_errors, mus1, sigmas1, mus2, sigmas2)
